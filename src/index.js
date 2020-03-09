199a3be5efd3109cbd2817cfcff281804fdb940b
Balaji Ganesan <balaji2104@gmail.com>
2020-03-08 22:59:07 -0700

    Adding tests for User Model

--- src/index.js
+++ src/index.js
@@ -1,50 +1,28 @@
-const express = require('express')
-require('./db/mongoose')
-const userRouter = require('./routers/user')
-const taskRouter = require('./routers/task')
+const app = require('./app')
 
-const app = express()
 const port = process.env.PORT
-const multer = require('multer')
-const upload = multer({
-    dest: 'images',
-    limits: {
-        fileSize: 1000000
-    },
-    fileFilter(req, file, cb) {
-        if(!file.originalname.match(/\.(doc|docx)$/)){
-            return cb(new Error('Please upload a Word Document.'))
-        }
-        cb(undefined, true)
-        // cb(new Error('File must be a PDF.'))
-        // cb(undefined, true)
-    }
-})
 
-app.post('/upload', upload.single('upload'), (req, res) => {
-    res.send()
-}, (error, req, res, next) => {
-    res.status(400).send({error: error.message})
-})
+// const upload = multer({
+//     dest: 'images',
+//     limits: {
+//         fileSize: 1000000
+//     },
+//     fileFilter(req, file, cb) {
+//         if(!file.originalname.match(/\.(doc|docx)$/)){
+//             return cb(new Error('Please upload a Word Document.'))
+//         }
+//         cb(undefined, true)
+//         // cb(new Error('File must be a PDF.'))
+//         // cb(undefined, true)
+//     }
+// })
 
-app.use(express.json())
-app.use(userRouter)
-app.use(taskRouter)
+// app.post('/upload', upload.single('upload'), (req, res) => {
+//     res.send()
+// }, (error, req, res, next) => {
+//     res.status(400).send({error: error.message})
+// })
 
 app.listen(port, () => {
     console.log('Server is up on ' + port)
 })
-
-const Task = require('./models/task')
-const User = require('./models/user')
-
-const main = async () => {
-    // const task = await Task.findById('5e5ca03ce2551b3582cad8a4')
-    // await task.populate('owner').execPopulate()
-    // console.log(task.owner)
-    const user = await User.findById('5e5c9f9e48ef3f34d9ddbb5e')
-    await user.populate('tasks').execPopulate()
-    console.log(user.tasks)
-}
-
-main()
---
+++ tests/user.test.js
@@ -0,0 +1,130 @@
+const request = require('supertest')
+const app = require('../src/app')
+const User = require('../src/models/user')
+const jwt = require('jsonwebtoken')
+const mongoose = require('mongoose')
+
+const userOneId = new mongoose.Types.ObjectId()
+
+const userOne = {
+    _id: userOneId,
+    name: 'GB TestUser',
+    email: 'gbtestuser@exampletest.com',
+    password: 'mypass123!',
+    tokens: [{
+        token: jwt.sign({_id: userOneId}, process.env.JWT_SECRET)
+    }]
+}
+
+beforeEach(async () => {
+    await User.deleteMany()
+    await new User(userOne).save()
+})
+
+test('Should sign up a new user', async () => {
+    const response = await request(app).post('/users').send({
+        name: 'Balaji',
+        email: 'balaji2104@gmail.com',
+        password: 'mypass123!'
+    }).expect(201)
+
+    //Assert that database was updated correctly
+    const user = await User.findById(response.body.user._id)
+    expect(user).not.toBeNull()
+
+    //Assertions about the reponse
+    expect(response.body).toMatchObject({
+        user: {
+            name: 'Balaji',
+            email: 'balaji2104@gmail.com'
+        },
+        token: user.tokens[0].token
+    })
+
+    expect(user.password).not.toBe('mypass123!')
+})
+
+test('Should login existing user', async () => {
+    const response = await request(app).post('/users/login').send({
+        email: userOne.email,
+        password: userOne.password
+    }).expect(200)
+
+    const user = await User.findById(response.body.user._id)
+    expect(response.body.token).toBe(user.tokens[1].token)
+})
+
+test('Should not login non-existing user', async () => {
+    await request(app).post('/users/login').send({
+        email: userOne.email,
+        password: 'randompass123!'
+    }).expect(400)
+})
+
+test('Should get profile for authenticated user', async () => {
+    await request(app)
+          .get('/users/me')
+          .set('Authorization', `Bearer ${userOne.tokens[0].token}`)
+          .send()
+          .expect(200)
+})
+
+test('Should not get profile for non-authenticated user', async () => {
+    await request(app)
+          .get('/users/me')
+          .send()
+          .expect(401)
+})
+
+test('Should delete profile for authenticated user', async () => {
+    const response = await request(app)
+          .delete('/users/me')
+          .set('Authorization', `Bearer ${userOne.tokens[0].token}`)
+          .send()
+          .expect(200)
+
+    //Assert that database was updated correctly
+    const user = await User.findById(userOneId)
+    expect(user).toBeNull()
+})
+
+test('Should not delete profile for non-authenticated user', async () => {
+    await request(app)
+          .delete('/users/me')
+          .send()
+          .expect(401)
+})
+
+test('Should upload avatar image', async () => {
+    await request(app)
+            .post('/users/me/avatar')
+            .set('Authorization', `Bearer ${userOne.tokens[0].token}`)
+            .attach('avatar', 'tests/fixtures/profile-pic.jpg')
+            .expect(200)
+
+    const user = await User.findById(userOneId)
+    expect(user.avatar).toEqual(expect.any(Buffer))
+})
+
+test('Should update valid user fields', async () => {
+    await request(app)
+          .patch('/users/me')
+          .set('Authorization', `Bearer ${userOne.tokens[0].token}`)
+          .send({
+              name: 'Sheetal Balaji'
+          })
+          .expect(200)
+    
+    const user = await User.findById(userOneId)
+    expect(user.name).toEqual('Sheetal Balaji')
+})
+
+test('Should not update invalid user fields', async () => {
+    await request(app)
+          .patch('/users/me')
+          .set('Authorization', `Bearer ${userOne.tokens[0].token}`)
+          .send({
+              location: 'San Jose'
+          })
+          .expect(400)
+})
